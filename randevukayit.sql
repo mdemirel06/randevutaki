7f546ca18bf97a25202e9013dddee8b8bda609a4
mdemirel06 <126471016+mdemirel06@users.noreply.github.com>
2023-03-31 09:04:56 +0300

    Add files via upload

---
+++ randevukayit.sql
@@ -0,0 +1,312 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 31, 2023 at 08:02 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `randevukayit`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `aeposta` varchar(255) NOT NULL,
+  `asifre` varchar(255) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`aeposta`, `asifre`) VALUES
+('admin@edoc.com', '123');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doktor`
+--
+
+CREATE TABLE `doktor` (
+  `docid` int(11) NOT NULL,
+  `docemail` varchar(255) DEFAULT NULL,
+  `docname` varchar(255) DEFAULT NULL,
+  `docpassword` varchar(255) DEFAULT NULL,
+  `docnic` varchar(15) DEFAULT NULL,
+  `doctel` varchar(15) DEFAULT NULL,
+  `specialties` int(11) DEFAULT NULL,
+  `zaman` int(11) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `doktor`
+--
+
+INSERT INTO `doktor` (`docid`, `docemail`, `docname`, `docpassword`, `docnic`, `doctel`, `specialties`, `zaman`) VALUES
+(4, 'emredrms@gmail.com', 'Emre Durmus', '123', '17984849848', '5515546252', 14, 30),
+(3, 'cagatay07@gmail.com', 'Cagatay Kurt', '123', '545454444', '595955595', 5, 20);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hasta`
+--
+
+CREATE TABLE `hasta` (
+  `pid` int(11) NOT NULL,
+  `pemail` varchar(255) DEFAULT NULL,
+  `pname` varchar(255) DEFAULT NULL,
+  `ppassword` varchar(255) DEFAULT NULL,
+  `paddress` varchar(255) DEFAULT NULL,
+  `pnic` varchar(15) DEFAULT NULL,
+  `pdob` date DEFAULT NULL,
+  `ptel` varchar(15) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `hasta`
+--
+
+INSERT INTO `hasta` (`pid`, `pemail`, `pname`, `ppassword`, `paddress`, `pnic`, `pdob`, `ptel`) VALUES
+(4, 'yilmaz@gmail.com', 'Ali Yilmaz', '123', 'sultanorhan mah. 258 sok. Gebze/Kocaeli', '1795668988', '2002-08-15', '0596669668'),
+(3, 'hasankrt@gmail.com', 'Hasan kurt', '123', 'gebze tatlikuyu', '1905', '2002-02-20', '0596669666');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `klinik`
+--
+
+CREATE TABLE `klinik` (
+  `id` int(11) NOT NULL,
+  `klinikler` varchar(50) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `klinik`
+--
+
+INSERT INTO `klinik` (`id`, `klinikler`) VALUES
+(1, 'Aile Hekimligi'),
+(2, 'Beyin ve Sinir Cerrahisi'),
+(3, 'Cocuk Cerrahisi'),
+(4, 'Cocuk Sagligi ve Hastaliklari'),
+(5, 'Kardiyoloji'),
+(6, 'Cildiye'),
+(7, 'Enfeksiyon Hastaliklari'),
+(8, 'Fiziksel Tip ve Rehabilitasyon'),
+(9, 'Gastroenteroloji'),
+(10, 'Genel Cerrahi'),
+(11, 'Gögüs Hastaliklari'),
+(12, 'Göz Hastaliklari'),
+(13, 'Dermatoloji'),
+(14, 'Dahiliye'),
+(15, 'Kadin Hastaliklari ve Dogum'),
+(16, 'Kalp ve Damar Cerrahisi'),
+(17, 'Kulak Burun Bogaz Hastaliklari'),
+(18, 'Kulak Burun Bo?az Hastal?klar?'),
+(19, 'Nöroloji'),
+(20, 'Ortopedi ve Travmatoloji'),
+(21, 'Psikiyatri'),
+(22, 'Üroloji'),
+(39, 'Patoloji'),
+(40, 'Pharmacology'),
+(41, 'Physical medicine and rehabilitation'),
+(42, 'Plastic surgery'),
+(43, 'Podiatric Medicine'),
+(44, 'Podiatric Surgery'),
+(45, 'Psychiatry'),
+(46, 'Public health and Preventive Medicine'),
+(47, 'Radiology'),
+(48, 'Radiotherapy'),
+(49, 'Respiratory medicine'),
+(50, 'Rheumatology'),
+(51, 'Stomatology'),
+(52, 'Thoracic surgery'),
+(53, 'Tropical medicine'),
+(54, 'Urology'),
+(55, 'Vascular surgery'),
+(56, 'Venereology');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `randevu`
+--
+
+CREATE TABLE `randevu` (
+  `appoid` int(11) NOT NULL,
+  `pid` int(11) DEFAULT NULL,
+  `apponum` int(11) DEFAULT NULL,
+  `scheduleid` int(11) DEFAULT NULL,
+  `appodate` date DEFAULT NULL,
+  `rndtime` time DEFAULT NULL,
+  `bitis` date DEFAULT NULL,
+  `docid` int(11) DEFAULT NULL,
+  `klinikid` int(11) DEFAULT NULL,
+  `rndurum` int(11) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `randevu`
+--
+
+INSERT INTO `randevu` (`appoid`, `pid`, `apponum`, `scheduleid`, `appodate`, `rndtime`, `bitis`, `docid`, `klinikid`, `rndurum`) VALUES
+(67, 4, 1, 49, '2023-03-27', '12:15:00', '2023-03-29', 4, 1, 3),
+(72, 4, 1, 50, '2023-03-31', '13:35:00', '2023-04-05', 3, 5, 0),
+(77, 4, 4, 49, '2023-04-07', '10:15:00', '2023-04-08', 4, 1, 0),
+(80, 3, 3, 49, '2023-04-07', '09:15:00', '2023-04-08', 4, 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `takvim`
+--
+
+CREATE TABLE `takvim` (
+  `scheduleid` int(11) NOT NULL,
+  `docid` varchar(255) DEFAULT NULL,
+  `klinik_id` int(11) DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `scheduledate` datetime DEFAULT NULL,
+  `scheduletime` time DEFAULT NULL,
+  `nop` int(11) DEFAULT NULL,
+  `bitis` datetime DEFAULT NULL,
+  `Durum` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `takvim`
+--
+
+INSERT INTO `takvim` (`scheduleid`, `docid`, `klinik_id`, `title`, `scheduledate`, `scheduletime`, `nop`, `bitis`, `Durum`) VALUES
+(49, '4', 1, 'hasta', '2023-04-03 08:30:00', '00:00:00', 5, '2023-04-07 18:00:00', 1),
+(50, '3', 5, 'assa', '2023-03-28 15:51:00', '00:00:00', 3, '2023-04-05 15:51:00', 1),
+(48, '3', 5, 'Hasta', '2023-03-28 15:51:00', '00:00:00', 2, '2023-04-05 15:51:00', 1),
+(51, '3', 5, 'imran', '2023-04-04 20:39:00', '00:00:00', 4, '2023-04-07 20:39:00', 1),
+(52, '3', 5, 'Klinik Deneme', '2023-04-04 22:30:00', '00:00:00', 4, '2023-04-12 22:30:00', 1),
+(54, '3', 5, 'cardo2', '2023-04-03 23:19:00', '00:00:00', 1, '2023-04-13 23:19:00', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `webkullanici`
+--
+
+CREATE TABLE `webkullanici` (
+  `email` varchar(255) NOT NULL,
+  `kullanicitipi` char(1) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `webkullanici`
+--
+
+INSERT INTO `webkullanici` (`email`, `kullanicitipi`) VALUES
+('admin@edoc.com', 'a'),
+('yilmaz@gmail.com', 'p'),
+('emhashenudara@gmail.com', 'p'),
+('emredrms@gmail.com', 'd'),
+('cagatay07@gmail.com', 'd'),
+('hasankrt@gmail.com', 'p');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`aeposta`);
+
+--
+-- Indexes for table `doktor`
+--
+ALTER TABLE `doktor`
+  ADD PRIMARY KEY (`docid`),
+  ADD KEY `specialties` (`specialties`);
+
+--
+-- Indexes for table `hasta`
+--
+ALTER TABLE `hasta`
+  ADD PRIMARY KEY (`pid`);
+
+--
+-- Indexes for table `klinik`
+--
+ALTER TABLE `klinik`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `randevu`
+--
+ALTER TABLE `randevu`
+  ADD PRIMARY KEY (`appoid`),
+  ADD KEY `pid` (`pid`),
+  ADD KEY `scheduleid` (`scheduleid`);
+
+--
+-- Indexes for table `takvim`
+--
+ALTER TABLE `takvim`
+  ADD PRIMARY KEY (`scheduleid`),
+  ADD KEY `docid` (`docid`);
+
+--
+-- Indexes for table `webkullanici`
+--
+ALTER TABLE `webkullanici`
+  ADD PRIMARY KEY (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `doktor`
+--
+ALTER TABLE `doktor`
+  MODIFY `docid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `hasta`
+--
+ALTER TABLE `hasta`
+  MODIFY `pid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `randevu`
+--
+ALTER TABLE `randevu`
+  MODIFY `appoid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=81;
+
+--
+-- AUTO_INCREMENT for table `takvim`
+--
+ALTER TABLE `takvim`
+  MODIFY `scheduleid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
